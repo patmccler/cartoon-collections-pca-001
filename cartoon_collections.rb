d2a84a042e6cbb9f5736454645cba5f519e8155d
Pat McClernan <patmccler@gmail.com>
2020-04-30 10:24:26 -0400

    build methods out to pass tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+    dwarves.each_with_index do |dwarf, i|
+      puts "#{i + 1}. #{dwarf}"
+    end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet veggies
+  veggies.map do |veggie|
+    veggie.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls planeteers
+  planeteers.any? do|planeteer|
+    planeteer.size > 4
+  end
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese maybe_cheeses
   cheese_types = ["cheddar", "gouda", "camembert"]
+  maybe_cheeses.find do |maybe|
+    cheese_types.include? maybe
+  end
 end
